d3c5719e3280d24dcd935df3bee4e70ad004fbbd
edieguez <dieguezemmanuel@gmail.com>
2024-05-01 17:59:46 -0600

    Added test to reverse Strings and numbers

---
+++ src/test/java/com/artemisa/top10algorithms/ReverseTest.java
@@ -0,0 +1,50 @@
+package com.artemisa.top10algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseTest {
+
+    @Test
+    void reverseStringTest() {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String reversedAlphabet = "zyxwvutsrqponmlkjihgfedcba";
+
+        assertEquals(reverseStringA(alphabet), reversedAlphabet);
+        assertEquals(reverseStringB(alphabet), reversedAlphabet);
+    }
+
+    @Test
+    void reverseNumberTest() {
+        int number = 123456789;
+        int reversedNumber = 987654321;
+
+        assertEquals(reverseNumber(number), reversedNumber);
+    }
+
+    private String reverseStringA(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    private String reverseStringB(String string) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = string.length() - 1; i >= 0; i--) {
+            builder.append(string.charAt(i));
+        }
+
+        return builder.toString();
+    }
+
+    private int reverseNumber(int number) {
+        int reversed = 0;
+
+        while (number != 0) {
+            reversed = reversed * 10 + number % 10;
+            number /= 10;
+        }
+
+        return reversed;
+    }
+}
